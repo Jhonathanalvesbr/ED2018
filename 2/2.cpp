fe17837de434ff13417c1ff40e7b104b77960d37
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2018-02-21 19:52:16 -0300

    Exercícios

--- 2/2.cpp
+++ 2/2.cpp
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char string[10] = "";
+	char string[11] = "";
 	gets(string);
 	int x = strlen(string);
-	for(x;x >= 0;x--)
+	for(x=x-1;x >= 0;x--)
 	{
 		printf("%c\n", string[x]);
 	} 
